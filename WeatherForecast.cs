01f8d30cab4311bfea96db52e275a6eea99703b1
tdunham <tdunham@USCONTDUNHAM01>
2021-01-04 16:19:32 -0500

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace configurator_with_post_message_redux
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
